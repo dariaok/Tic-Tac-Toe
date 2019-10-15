5e3cb2b5c29b5cf685740f5c6bbfcea2ad4c873c
espilya <espilya@gmail.com>
2019-10-15 18:53:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -126,22 +126,19 @@ void board()
     cout << "\n\n\tTic Tac Toe\n\n";
 
     cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl;
-    cout << endl;
 
-    cout << "     │     │     " << endl;
-    cout << "  " << square[1] << "  │  " << square[2] << "  │  " << square[3] << endl;
+    cout << "\t" << "┌───┬───┬───┐" << endl;
+    cout << "\t" << "│ " << square[1] << " │ " << square[2] << " │ " << square[3] << " │" << endl;
 
-    cout << "─────┼─────┼─────" << endl;
-    cout << "     │     │     " << endl;
+    cout << "\t" << "├───┼───┼───┤" << endl;
 
-    cout << "  " << square[4] << "  │  " << square[5] << "  │  " << square[6] << endl;
+    cout << "\t" << "│ " << square[4] << " │ " << square[5] << " │ " << square[6] << " │" << endl;
 
-    cout << "─────┼─────┼─────" << endl;
-    cout << "     │     │     " << endl;
+    cout << "\t" << "├───┼───┼───┤" << endl;
 
-    cout << "  " << square[7] << "  │  " << square[8] << "  │  " << square[9] << endl;
+    cout << "\t" << "│ " << square[7] << " │ " << square[8] << " │ " << square[9] << " │" << endl;
 
-    cout << "     │     │     " << endl << endl;
+    cout << "\t" << "└───┴───┴───┘" << endl << endl;
 }
 
 /*******************************************************************
