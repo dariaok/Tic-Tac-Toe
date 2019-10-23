1953e84df1a35d3661d2508d0922b6fc85c35a9d
Abhishek Khurana <abhishekkhurana12345@gmail.com>
2019-10-23 17:10:46 +0530

    Signed-off-by: Abhishek Khurana <abhishekkhurana12345@gmail.com>

--- main.cpp
+++ main.cpp
@@ -21,33 +21,8 @@ int main()
 
         mark=(player == 1) ? 'X' : 'O';
 
-        if (choice == 1 && square[1] == '1')
-
-            square[1] = mark;
-        else if (choice == 2 && square[2] == '2')
-
-            square[2] = mark;
-        else if (choice == 3 && square[3] == '3')
-
-            square[3] = mark;
-        else if (choice == 4 && square[4] == '4')
-
-            square[4] = mark;
-        else if (choice == 5 && square[5] == '5')
-
-            square[5] = mark;
-        else if (choice == 6 && square[6] == '6')
-
-            square[6] = mark;
-        else if (choice == 7 && square[7] == '7')
-
-            square[7] = mark;
-        else if (choice == 8 && square[8] == '8')
-
-            square[8] = mark;
-        else if (choice == 9 && square[9] == '9')
-
-            square[9] = mark;
+		if ((choice > 0 && choice < 10) && square[choice] == '0' + choice)
+			square[choice] = mark;
         else
         {
             cout<<"Invalid move ";
@@ -128,20 +103,13 @@ void board()
     cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl;
     cout << endl;
 
-    cout << "     │     │     " << endl;
-    cout << "  " << square[1] << "  │  " << square[2] << "  │  " << square[3] << endl;
+	for (char i = 0; i < 3; ++i) {
+		cout << "     │     │     " << endl;
+		cout << "  " << square[i * 3 + 1] << "  │  " << square[i * 3 + 2] << "  │  " << square[i * 3 + 3] << endl;
+		if(i<2)cout << "─────┼─────┼─────" << endl;
+		else cout << "     │     │     " << endl << endl;
+	}
 
-    cout << "─────┼─────┼─────" << endl;
-    cout << "     │     │     " << endl;
-
-    cout << "  " << square[4] << "  │  " << square[5] << "  │  " << square[6] << endl;
-
-    cout << "─────┼─────┼─────" << endl;
-    cout << "     │     │     " << endl;
-
-    cout << "  " << square[7] << "  │  " << square[8] << "  │  " << square[9] << endl;
-
-    cout << "     │     │     " << endl << endl;
 }
 
 /*******************************************************************
